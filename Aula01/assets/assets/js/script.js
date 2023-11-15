5fd4e416cc6b0727cce65e3c58523a8fa4269776
Bruno-Assumpcao-Motta <bruno.assump@gmail.com>
2023-11-15 19:42:25 -0300

    construindo as estruturas

---
+++ Aula01/assets/assets/js/script.js
@@ -0,0 +1,15 @@
+var valorEmDolar = 14;
+var cotacaoDolar = 5.32;
+var valorEmReal = valorEmDolar * cotacaoDolar;
+valorEmReal = valorEmReal.toFixed(2);
+
+alert(`${"Preciso de R$ " + valorEmReal} para converter ${"US$ " + valorEmDolar }`);
+
+
+
+var valorEmEuro = 80;
+var cotacaoEuro = 5.29;
+var valorEmReal = valorEmEuro * cotacaoEuro;
+valorEmReal = valorEmReal.toFixed(2);
+
+alert(`${"Preciso de R$ " + valorEmReal} para converter ${"EUR " + valorEmEuro}`);
