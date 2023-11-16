6c61f5bf261326a4532d688a15270ccb8e825feb
Bruno-Assumpcao-Motta <bruno.assump@gmail.com>
2023-11-16 19:03:13 -0300

    Update script2.js

--- Aula02/assets/js/script2.js
+++ Aula02/assets/js/script2.js
@@ -1,13 +1,13 @@
-var numeroSecreto = 581;
+var numeroSecreto = parseInt(Math.random() * 1001);
 
 while(chute != numeroSecreto) {
-    var chute = prompt('Digite o número entre 1 e 1000')
+    var chute = prompt('Digite o número entre 0 e 1000')
 
     if(chute == numeroSecreto) {
     alert('Acertou!')
     } else if(chute > numeroSecreto) {
-    alert('Errou... o número secreto é menor')
+    alert('Errou! Tente novamente. O número secreto é menor')
     } else if(chute < numeroSecreto) {
-    alert('Errou... o número secreto é maior')
+    alert('Errou! Tente novamente. O número secreto é maior')
     }
 }
