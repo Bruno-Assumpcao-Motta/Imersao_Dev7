b64d71379ef13064e7c77677e3f0bd93e4369710
Bruno-Assumpcao-Motta <bruno.assump@gmail.com>
2023-11-16 19:34:54 -0300

    Update script3.js

---
+++ Aula03/assets/js/script3.js
@@ -0,0 +1,10 @@
+var filmeRafa = "https://upload.wikimedia.org/wikipedia/pt/thumb/1/1b/Schoolrockposter.jpg/210px-Schoolrockposter.jpg";
+
+var filmePaulo = "https://media.fstatic.com/DjhdaLmpvfJaleMFTvM2aN1-zq8=/322x478/smart/filters:format(webp)/media/movies/covers/2016/08/chegada_2-1.jpg";
+
+var filmeGui = "https://www.rbsdirect.com.br/imagesrc/35798348.jpg?version=1575255600";
+
+
+document.write("<img src=" + filmeRafa + ">");
+document.write("<img src=" + filmePaulo + ">");
+document.write("<img src=" + filmeGui + ">");
