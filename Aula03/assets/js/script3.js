e78cbf561de9dd7f73aab4a5084806a7701ca417
Bruno-Assumpcao-Motta <bruno.assump@gmail.com>
2023-11-20 19:51:22 -0300

    f

--- Aula03/assets/js/script3.js
+++ Aula03/assets/js/script3.js
@@ -1,9 +1,9 @@
-var listaFilmes = []
-listaFilmes[0] = "https://upload.wikimedia.org/wikipedia/pt/thumb/1/1b/Schoolrockposter.jpg/210px-Schoolrockposter.jpg";
-listaFilmes[1] = "https://media.fstatic.com/DjhdaLmpvfJaleMFTvM2aN1-zq8=/322x478/smart/filters:format(webp)/media/movies/covers/2016/08/chegada_2-1.jpg";
-listaFilmes[2] = "https://www.rbsdirect.com.br/imagesrc/35798348.jpg?version=1575255600";
+var listaFilmes = ["https://upload.wikimedia.org/wikipedia/pt/thumb/1/1b/Schoolrockposter.jpg/210px-Schoolrockposter.jpg","https://media.fstatic.com/DjhdaLmpvfJaleMFTvM2aN1-zq8=/322x478/smart/filters:format(webp)/media/movies/covers/2016/08/chegada_2-1.jpg","https://cdn.awsli.com.br/600x700/1610/1610163/produto/193840003/poster-homem-aranha-atraves-do-aranhaverso-a-2015d161.jpg","https://media.farolnoticias.com.br/2021/10/241438561_10209256829094888_8179075484310727610_n.jpg","https://img.elo7.com.br/product/original/2679A20/big-poster-filme-matrix-lo03-tamanho-90x60-cm-geek.jpg"];
 
 
-document.write("<img src=" + listaFilmes[0] + ">");
-document.write("<img src=" + listaFilmes[1] + ">");
-document.write("<img src=" + listaFilmes[2] + ">");
+for (var i=0; i < listaFilmes.length; i++) {
+    document.write("<img src=" + listaFilmes[i] + ">");
+   
+}
+
+
