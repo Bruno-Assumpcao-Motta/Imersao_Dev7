c8a148308c9cc727cfa3ad65e43a9fbc155989ef
Bruno-Assumpcao-Motta <bruno.assump@gmail.com>
2023-11-20 19:03:03 -0300

    Update script3.js

--- Aula03/assets/js/script3.js
+++ Aula03/assets/js/script3.js
@@ -1,10 +1,9 @@
-var filmeRafa = "https://upload.wikimedia.org/wikipedia/pt/thumb/1/1b/Schoolrockposter.jpg/210px-Schoolrockposter.jpg";
+var listaFilmes = []
+listaFilmes[0] = "https://upload.wikimedia.org/wikipedia/pt/thumb/1/1b/Schoolrockposter.jpg/210px-Schoolrockposter.jpg";
+listaFilmes[1] = "https://media.fstatic.com/DjhdaLmpvfJaleMFTvM2aN1-zq8=/322x478/smart/filters:format(webp)/media/movies/covers/2016/08/chegada_2-1.jpg";
+listaFilmes[2] = "https://www.rbsdirect.com.br/imagesrc/35798348.jpg?version=1575255600";
 
-var filmePaulo = "https://media.fstatic.com/DjhdaLmpvfJaleMFTvM2aN1-zq8=/322x478/smart/filters:format(webp)/media/movies/covers/2016/08/chegada_2-1.jpg";
 
-var filmeGui = "https://www.rbsdirect.com.br/imagesrc/35798348.jpg?version=1575255600";
-
-
-document.write("<img src=" + filmeRafa + ">");
-document.write("<img src=" + filmePaulo + ">");
-document.write("<img src=" + filmeGui + ">");
+document.write("<img src=" + listaFilmes[0] + ">");
+document.write("<img src=" + listaFilmes[1] + ">");
+document.write("<img src=" + listaFilmes[2] + ">");
