328dcf6ca5afcd0b236b888e92067d76f8f21ac1
Bruno-Assumpcao-Motta <bruno.assump@gmail.com>
2023-11-21 19:32:26 -0300

    Update script4.js

--- Aula04/assets/js/script4.js
+++ Aula04/assets/js/script4.js
@@ -1,5 +1,6 @@
 function adicionarFilme() {
     var filmeFavorito = document.getElementById('filme').value
     var elementoListaFilmes = document.getElementById('listaFilmes')
-    elementoListaFilmes.innerHTML = '<img src=' + filmeFavorito + '>'
+    elementoListaFilmes.innerHTML = elementoListaFilmes.innerHTML + '<img src=' + filmeFavorito + '>'
+    document.getElementById('filme').value = ''
 }
