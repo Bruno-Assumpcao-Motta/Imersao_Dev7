60d0bcacb3444b8e2f5536efe3d5a208a60195bf
Bruno-Assumpcao-Motta <bruno.assump@gmail.com>
2023-11-21 20:45:21 -0300

    o

--- Aula05/assets/js/script5.js
+++ Aula05/assets/js/script5.js
@@ -1,11 +1,50 @@
-function adicionarVitoria(){
+var paulo = {
+    nome:'Paulo',
+    vitoria:0,
+    empate:0,
+    derrota:0,
+    pontos:0
+};
+var rafa = {
+    nome:'Rafa',
+    vitoria:0,
+    empate:0,
+    derrota:0,
+    pontos:0
+
+};
+
+var bruno = {
+    nome:'Bruno',
+    vitoria:0,
+    empate:0,
+    derrota:0,
+    pontos:0
+
+};
+
+var elementoTabela = document.getElementById('tabelaJogadores')
+elementoTabela.innerHTML = `
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td><button onClick="adicionarVitoria()">Vitória</button></td>
+        <td><button onClick="adicionarEmpate()">Empate</button></td>
+        <td><button onClick="adicionarDerrota()">Derrota</button></td>
+    </tr>
+` 
+
+function adicionarVitoria() {
+
+} 
+
+function adicionarEmpate() {
 
 }
 
-function adicionarEmpate(){
+function adicionarDerrota() {
 
-}
-
-function adicionarDerrota(){
-    
 }
